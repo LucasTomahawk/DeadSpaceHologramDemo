6f3ad14c9463aa1f44e929292a2473cc5e7d5b60
Lucas <lucast2214@gmail.com>
2021-04-20 11:32:05 -0500

    Working Door
    Working Door & button presses

---
+++ Assets/_DeadSpaceHologramDemo/Holograms/Door/Scripts/DoorTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public OpenDoor Door;
+    bool isInRange = false;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        isInRange = true;
+        Debug.Log("In Trigger");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isInRange = false;
+        Debug.Log("NOT in Trigger");
+        Door.ButtonTryToCloseDoors(); // Close Door on Trigger Exit
+    }
+
+    private void Update()
+    {
+        if (isInRange = true & Input.GetKeyDown(KeyCode.E)) // Open Door Press
+        {
+            Door.ButtonTryToOpenDoors();
+        }
+    }
+}
---
+++ Assets/_DeadSpaceHologramDemo/Holograms/Door/Scripts/OpenDoor.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public GameObject doorTrigger;
+    public Transform leftDoor;
+    public Transform rightDoor;
+    public float moveSpeed = 1;
+    public float sizeOfDoorInX = 1;
+    public float amountOfDoorInWall = 0.9f;
+
+    private Vector3 leftDoorCloseTarget;
+    private Vector3 rightDoorCloseTarget;
+    private Vector3 leftDoorOpenTarget;
+    private Vector3 rightDoorOpenTarget;
+    private float startTime;
+    private float totalDistanceToCover;
+    private bool areDoorsOpen = false;
+    private bool isDoorOpenButtonPressed = false;
+    private bool isDoorCloseButtonPressed = false;
+
+    void Start()
+    {
+        SetInitialReferences();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        OpenDoors();
+        CloseDoors();
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //ButtonTryToOpenDoors();
+        }
+
+
+    }
+
+    void SetInitialReferences()
+    {
+        leftDoorCloseTarget = leftDoor.localPosition;
+        rightDoorCloseTarget = rightDoor.localPosition;
+
+        leftDoorOpenTarget = new Vector3(
+            leftDoor.localPosition.x - (sizeOfDoorInX * amountOfDoorInWall),
+            leftDoor.localPosition.y,
+            leftDoor.localPosition.z);
+
+        rightDoorOpenTarget = new Vector3(
+            rightDoor.localPosition.x + (sizeOfDoorInX * amountOfDoorInWall),
+            rightDoor.localPosition.y,
+            rightDoor.localPosition.z);
+
+        totalDistanceToCover = Vector3.Distance(leftDoorCloseTarget, leftDoorOpenTarget);
+    }
+
+    void OpenDoors()
+    {
+        if (areDoorsOpen || !isDoorOpenButtonPressed)
+        {
+            return;
+        }
+
+        float distanceCovered = (Time.time - startTime) * moveSpeed;
+        float fractionOfJourney = distanceCovered / totalDistanceToCover;
+        leftDoor.localPosition = Vector3.Lerp(leftDoor.localPosition, leftDoorOpenTarget, fractionOfJourney);
+        rightDoor.localPosition = Vector3.Lerp(rightDoor.localPosition, rightDoorOpenTarget, fractionOfJourney);
+
+        if (Mathf.Approximately(rightDoor.localPosition.x, rightDoorOpenTarget.x))
+        {
+            Debug.Log("Doors Opened");
+            areDoorsOpen = true;
+            isDoorOpenButtonPressed = false;
+        }
+    }
+
+    void CloseDoors()
+    {
+        if (!areDoorsOpen || !isDoorCloseButtonPressed)
+        {
+            return;
+        }
+
+        float distanceCovered = (Time.time - startTime) * moveSpeed;
+        float fractionOfJourney = distanceCovered / totalDistanceToCover;
+        leftDoor.localPosition = Vector3.Lerp(leftDoor.localPosition, leftDoorCloseTarget, fractionOfJourney);
+        rightDoor.localPosition = Vector3.Lerp(rightDoor.localPosition, rightDoorCloseTarget, fractionOfJourney);
+
+        if (Mathf.Approximately(rightDoor.localPosition.x, rightDoorCloseTarget.x))
+        {
+            Debug.Log("Doors Closed");
+            areDoorsOpen = false;
+            isDoorCloseButtonPressed = false;
+        }
+    }
+
+    public void ButtonTryToOpenDoors()
+    {
+        if (areDoorsOpen)
+        {
+            return;
+        }
+        startTime = Time.time;
+        isDoorOpenButtonPressed = true;
+    }
+
+    public void ButtonTryToCloseDoors()
+    {
+        if (!areDoorsOpen)
+        {
+            return;
+        }
+        startTime = Time.time;
+        isDoorCloseButtonPressed = true;
+    }
+}
